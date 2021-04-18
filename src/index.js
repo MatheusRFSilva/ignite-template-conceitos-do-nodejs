c451a31ee68d9f1e2700982c9903b605ff89737d
Matheus Silva <matheusuniversal@yahoo.com.br>
2021-04-18 13:43:05 -0300

    Finalizando rota de criacao de usuario

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+
+
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, username } = request.body;
+
+  const userExists = users.some(user => {
+    return (user.username === username);
+  });
+  if (userExists) {
+    return response.status(400).json({ error: "Este usuario ja existe !" });
+  }
+  const id = uuidv4();
+
+  users.push({
+    id,
+    name,
+    username,
+    todos: []
+  });
+
+  return response.status(201).send();
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
