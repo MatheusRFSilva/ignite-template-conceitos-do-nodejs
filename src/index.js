d2e39f58ad1acff0e1701c1406a4db81764e0e68
Matheus Silva <matheusuniversal@yahoo.com.br>
2021-04-18 15:12:07 -0300

    Finalizando desafio

--- src/index.js
+++ src/index.js
@@ -12,7 +12,18 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
 
+  const user = users.find(user => {
+    return (user.username === username);
+  });
+  if (!user) {
+    return response.status(404).json({ error: "Este usuario nao existe !" });
+  }
+  request.user = user;
+
+
+  return next();
 
 }
 
@@ -27,36 +38,87 @@ app.post('/users', (request, response) => {
     return response.status(400).json({ error: "Este usuario ja existe !" });
   }
   const id = uuidv4();
-
-  users.push({
+  const user = {
     id,
     name,
     username,
     todos: []
-  });
+  }
+  users.push(user);
 
-  return response.status(201).send();
+  return response.status(201).json(user);
 
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+
+  return response.status(201).json(user.todos);
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+
+  const todo = {
+    id: uuidv4(),
+    done: false,
+    title,
+    deadline,
+    created_at: new Date()
+
+  };
+
+  user.todos.push(todo);
+  return response.status(201).json(todo);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Tarefa nao localizada!" });
+  }
+
+  todo.title = title;
+  todo.deadline = deadline;
+  return response.json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Tarefa nao localizada!" });
+  }
+
+  todo.done = true;
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Tarefa nao localizada!" });
+  }
+  user.todos.splice(todo, 1);
+  return response.status(204).send();
 });
 
 module.exports = app;
